c75e8dace14ef4efcc2a9740b7ab73db811ac514
Antonin Mutzenberg <antonin.mutzenberg@gmail.com>
2019-03-11 09:32:39 +0100

    Ajout des fixtures aver Faker

---
+++ src/AppBundle/DataFixtures/ORM/UcapeFixtures.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AppBundle\Entity\Classe;
+use AppBundle\Entity\Eleve;
+use AppBundle\Entity\Etablissement;
+use AppBundle\Entity\Examinateur;
+use AppBundle\Entity\Langue;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Pays;
+use AppBundle\Entity\Proposition;
+
+class UcapeFixtures implements FixtureInterface, ContainerAwareInterface
+{
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $user = $userManager->createUser();
+        $user->setUsername('admin');
+        $user->setEmail('technicien@lyceestvincent.fr');
+        $user->setPlainPassword('admin');
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_SUPER_ADMIN'));
+
+        $userManager->updateUser($user, true);
+
+        
+        for ($l=1;$l <=8; $l++) {
+            $pays = new Pays();
+            $pays->setLibelle($faker->country());
+
+            $manager->persist($pays);
+        }
+
+        for ($x=1; $x<=8;$x++) {
+
+            $userRandom = new User();
+            $userRandom->setUsername($faker->username());;
+            $userRandom->setEmail($faker->email());
+            $userRandom->setPlainPassword($faker->password());
+            $userRandom->setEnabled($faker->boolean());
+
+            $manager->persist($userRandom);
+        }
+        for ($l=1;$l <=150; $l++)
+        {
+            $eleve = new Eleve();
+            $eleve->setNom($faker->lastName());
+            $eleve->setPrenom($faker->firstName());
+            $eleve->setSexe($faker->randomElement($array = array ('g','f')));
+            $eleve->setDateNaissance($faker->dateTime());
+            $eleve->setPromo($faker->year($max = 'now'));
+            $eleve->setEmail($faker->email());
+            $eleve->setEmailParent($faker->email());
+            $eleve->setMdp($faker->password());
+            $eleve->setCommentaireGeneral($faker->text($maxNbChars=60));
+            $eleve->setTerreDesLangues($faker->boolean());
+            $eleve->setCommentaireChoix($faker->text($maxNbChars=50));
+            $eleve->setVisaParent($faker->boolean());
+            $eleve->setUE2Date($faker->dateTime());
+            $eleve->setUE2ThemeDossier($faker->catchPhrase());
+            $eleve->setUE2Note($faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 20));
+            $eleve->setUE2Appreciation($faker->word());
+            $eleve->setType($faker->boolean());
+
+            $manager->persist($eleve);
+
+            if ($eleve->getType() == true)
+            {
+                $eleve->setUE1Date($faker->dateTime());
+                $eleve->setUE1Note($faker->numberBetween($min = 0, $max = 20));
+                $eleve->setUE1Appreciation($faker->randomElement($array = array ('Bon travail, peut être amélioré','Elève concerné, apprentissage à consolider','Très bon élève, des résultats excellents','Elève dissipé, manque de connaissance, resaisissez-vous','Elève nul')));
+                $eleve->setObtentionDiplome($faker->boolean());
+                $eleve->setMention($faker->randomElement($array = array ('Bien','Très bien','Excellent')));
+                $eleve->setCommentaire($faker->text());
+            }
+        }
+        for ($i = 0; $i < 10 ; $i++) {
+            $etablissement = new Etablissement();
+            $etablissement->setLibelle($faker->company());
+            $etablissement->setNom($faker->company());
+            $etablissement->setTelephone($faker->e164PhoneNumber());
+            $etablissement->setEmail($faker->email());
+            $etablissement->setResponsable($faker->name());
+            $etablissement->setNumero($faker->numberBetween($min=0, $max=100));
+            $etablissement->setRue($faker->numberBetween($min=0, $max=100));
+            $etablissement->setVille($faker->city());
+            
+            $manager->persist($etablissement);
+        }
+
+        $arrayClasse = ["Seconde 3", "Seconde 2", "Seconde 1", "Première 3", "Première 2", "Première 1", "Terminale 3", "Terminale 2", "Terminale 1", "Terminale 4"];
+        for ($j = 0; $j < 10 ; $j++) {
+            $classe = new Classe();
+            $classe->setLibelle($arrayClasse[$j]);
+
+            $manager->persist($classe);
+        }
+
+        for ($j = 0; $j < 10 ; $j++) {
+            $examinateur = new Examinateur();
+            $examinateur->setNom($faker->lastName());
+            $examinateur->setPrenom($faker->firstName());
+            $examinateur->setTelephone($faker->e164PhoneNumber());
+
+            $manager->persist($examinateur);
+        }
+        
+        $arrayLangue = ["Anglais","Espagnol","Allemand", "Italien","Néerlandais"];
+        for ($j = 0; $j < 5 ; $j++) {
+            $langue = new Langue();
+            $langue->setLibelle($arrayLangue[$j]);
+            
+            $manager->persist($langue);
+        }
+
+        $manager->flush();
+    }
+}
